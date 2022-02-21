1f9c6c646761d16d8d6b912de3e67f39c9fa9ba6
Het Thakkar <hbthakkar2001@gmail.com>
2022-02-21 13:10:57 -0600

    Fixed test script with current repo

--- appTest.py
+++ appTest.py
@@ -1,26 +1,68 @@
 import unittest
 from flask import url_for
-from app import app
+from application import application
 
 class FlaskTestRoutes(unittest.TestCase):
     def test_index(self):
-        tester = app.test_client(self)
-        response = tester.get("/")
+        tester = application.test_client(self)
+        response = tester.get("/", follow_redirects=True)
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
 
     def test_sign_up(self):
-        tester = app.test_client(self)
-        response = tester.get("/sign_up")
+        tester = application.test_client(self)
+        response = tester.get("/sign_up", follow_redirects=True)
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
+    def test_user_reservation(self):
+        tester = application.test_client(self)
+        response = tester.get("/user_reservation", follow_redirects=True)
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
 
     def test_reservation(self):
-        tester = app.test_client(self)
-        response = tester.get("/reservation")
+        tester = application.test_client(self)
+        response = tester.get("/success", follow_redirects=True)
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
     
+    def test_reservation(self):
+        tester = application.test_client(self)
+        response = tester.get("/guest_reservation", follow_redirects=True)
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
+    def test_reservation(self):
+        tester = application.test_client(self)
+        response = tester.get("/forgot_password", follow_redirects=True)
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+    
+    
+class FlaskTestHTMLdata(unittest.TestCase):
+    def test_login_form(self):
+        tester = application.test_client(self)
+        response = tester.get("/")
+        #gets html page in text form
+        html = response.get_data(as_text=True)
+        # make sure all the fields are included
+        assert 'name = "email_login"' in html
+        assert 'name = "pass_login"' in html
+
+    def test_sign_up_form(self):
+        tester = application.test_client(self)
+        response = tester.get("/sign_up")
+        #gets html page in text form
+        html = response.get_data(as_text=True)
+        # make sure all the fields are included
+        assert 'name = "fname"' in html
+        assert 'name = "lname"' in html
+        assert 'name = "contact"' in html
+        assert 'name = "email"' in html
+        assert 'name = "pass_word"' in html
+        assert 'name = "pass_c"' in html
+        
 
 if __name__ == "__main__":
     unittest.main()
